1ef925fae7167758d6f0643254402748ad16db06
Matrix-X <hbj418@gmail.com>
2021-09-06 17:58:25 +0800

    {miniprogram} - create controller miniprogram
    {miniprogram} - controller init
    {miniprogram} - ass auth test unit

---
+++ services/miniprogramService.go
@@ -0,0 +1,31 @@
+package services
+
+import (
+	"github.com/ArtisanCloud/power-wechat/src/miniProgram"
+	"log"
+	"os"
+)
+
+var AppMiniProgram *miniProgram.MiniProgram
+
+func NewMiniMiniProgramService() (*miniProgram.MiniProgram, error) {
+	log.Printf("miniprogram app_id: %s", os.Getenv("miniprogram_app_id"))
+	app, err := miniProgram.NewMiniProgram(&miniProgram.UserConfig{
+
+		AppID:  os.Getenv("miniprogram_app_id"), // 小程序、公众号或者企业微信的appid
+		Secret: os.Getenv("miniprogram_secret"), // 商户号 appID
+
+		ResponseType: os.Getenv("array"),
+		Log: miniProgram.Log{
+			Level: "debug",
+			File:  "./wechat.log",
+		},
+
+		HttpDebug: true,
+		//Debug: true,
+		//"sandbox": true,
+
+	})
+
+	return app, err
+}
