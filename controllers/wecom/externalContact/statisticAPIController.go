f9cff02c97fd654d394b60cb8f77d1c079a7b5d5
Matrix-X <matrix-x@artisan-cloud.com>
2021-09-27 10:42:56 +0800

    feat(wecom) - external contact statistic

--- controllers/wecom/externalContact/statisticAPIController.go
+++ controllers/wecom/externalContact/statisticAPIController.go
@@ -1,10 +1,50 @@
 package externalContact
-import "github.com/gin-gonic/gin"
 
+import (
+  "github.com/ArtisanCloud/power-wechat/src/kernel/power"
+  "github.com/ArtisanCloud/power-wechat/src/work/externalContact/statistics/request"
+  "github.com/gin-gonic/gin"
+  "net/http"
+  "power-wechat-tutorial/services"
+)
+
+// 获取「联系客户统计」数据
+// https://work.weixin.qq.com/api/doc/90000/90135/92132
 func APIExternalContactGetUserBehaviorData(c *gin.Context) {
 
+  options := &request.RequestGetUserBehaviorData{
+    UserID:    []string{c.DefaultQuery("userID", "matrix-x")},
+    PartyID:   []int{1001},
+    StartTime: 1536508800,
+    EndTime:   1536595200,
+  }
+
+  res, err := services.WeComContactApp.ExternalContactStatistics.GetUserBehaviorData(options)
+
+  if err != nil {
+    panic(err)
+  }
+
+  c.JSON(http.StatusOK, res)
 }
 
 func APIExternalContactGroupChatStatistic(c *gin.Context) {
+  options := &request.RequestStatistic{
+    DayBeginTime: 1600272000,
+    DayEndTime:   1600444800,
+    OwnerFilter: &power.HashMap{
+      "userid_list": []string{c.DefaultQuery("userID", "matrix-x")},
+    },
+    OrderBy:  2,
+    OrderAsc: 0,
+    Offset:   0,
+    Limit:    1000,
+  }
+  res, err := services.WeComContactApp.ExternalContactStatistics.Statistic(options)
 
+  if err != nil {
+    panic(err)
+  }
+
+  c.JSON(http.StatusOK, res)
 }
