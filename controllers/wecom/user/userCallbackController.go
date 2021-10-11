35e8be5c79445981e87905167be41c779f5341f8
Matrix-X <matrix-x@artisan-cloud.com>
2021-10-12 00:29:30 +0800

    feat(wecom): user callback notify

--- controllers/wecom/user/userCallbackController.go
+++ controllers/wecom/user/userCallbackController.go
@@ -1,12 +1,14 @@
 package user
 
 import (
+	"github.com/ArtisanCloud/go-libs/fmt"
+  "github.com/ArtisanCloud/power-wechat/src/kernel"
   "github.com/ArtisanCloud/power-wechat/src/kernel/contract"
-  "github.com/ArtisanCloud/power-wechat/src/work/server/handlers"
-  "github.com/gin-gonic/gin"
-  "io/ioutil"
-  "net/http"
-  "power-wechat-tutorial/services"
+	"github.com/ArtisanCloud/power-wechat/src/work/server/handlers"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"net/http"
+	"power-wechat-tutorial/services"
 )
 
 // 回调配置
@@ -17,26 +19,33 @@ func CallbackVerify(c *gin.Context) {
 		panic(err)
 	}
 
-  text, _ := ioutil.ReadAll(rs.Body)
-  c.String(http.StatusOK, string(text))
+	text, _ := ioutil.ReadAll(rs.Body)
+	c.String(http.StatusOK, string(text))
 
 }
 
 // 回调配置
 // https://work.weixin.qq.com/api/doc/90000/90135/90930
 func CallbackNotify(c *gin.Context) {
-  closure := handlers.NewServerCallbackHandler()
-  closure.Callback = func(header contract.EventInterface, content interface{}) interface{} {
-    //fmt.Dump("payload", payload)
-    return "handle callback"
-  }
-  services.WeComContactApp.Server.Push(closure, 0)
 
-  rs, err := services.WeComContactApp.Server.Serve(c.Request)
+	closure := handlers.NewServerCallbackHandler()
+	closure.Callback = func(header contract.EventInterface, content interface{}) interface{} {
+		fmt.Dump("header", header, "content", content)
+		//return  "handle callback"
+		return  kernel.SUCCESS_EMPTY_RESPONSE
+
+  }
+	services.WeComContactApp.Server.Push(closure, 0)
+
+	rs, err := services.WeComContactApp.Server.Serve(c.Request)
+	if err != nil {
+		panic(err)
+	}
+
+  err = rs.Send(c.Writer)
+
   if err != nil {
     panic(err)
   }
-
-  c.JSON(http.StatusOK, rs)
 
 }
