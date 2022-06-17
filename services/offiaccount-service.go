01fa9be481bcb861c846ad6ced1b288545be69d9
Walle <walle@artisan-cloud.com>
2022-06-17 23:47:34 +0800

    feat(offcial): add Token and AES config

--- services/offiaccount-service.go
+++ services/offiaccount-service.go
@@ -1,40 +1,43 @@
 package services
 
 import (
-  "github.com/ArtisanCloud/PowerWeChat/v2/src/kernel"
-  "github.com/ArtisanCloud/PowerWeChat/v2/src/officialAccount"
-  "log"
-  "os"
-  "power-wechat-tutorial/config"
+	"github.com/ArtisanCloud/PowerWeChat/v2/src/kernel"
+	"github.com/ArtisanCloud/PowerWeChat/v2/src/officialAccount"
+	"log"
+	"os"
+	"power-wechat-tutorial/config"
 )
 
 var OfficialAccountApp *officialAccount.OfficialAccount
 
 func NewOfficialAccountAppService(conf *config.Configuration) (*officialAccount.OfficialAccount, error) {
-  log.Printf("officialAccount app_id: %s", os.Getenv("miniprogram_app_id"))
+	log.Printf("officialAccount app_id: %s", os.Getenv("miniprogram_app_id"))
 
-  var cache kernel.CacheInterface
-  if conf.MiniProgram.RedisAddr != "" {
-    cache = kernel.NewRedisClient(&kernel.RedisOptions{
-      Addr: conf.MiniProgram.RedisAddr,
-    })
-  }
+	var cache kernel.CacheInterface
+	if conf.MiniProgram.RedisAddr != "" {
+		cache = kernel.NewRedisClient(&kernel.RedisOptions{
+			Addr: conf.MiniProgram.RedisAddr,
+		})
+	}
 
-  app, err := officialAccount.NewOfficialAccount(&officialAccount.UserConfig{
+	app, err := officialAccount.NewOfficialAccount(&officialAccount.UserConfig{
 
-    AppID:  conf.OffiAccount.AppID,     // 小程序、公众号或者企业微信的appid
-    Secret: conf.OffiAccount.AppSecret, // 商户号 appID
+		AppID:  conf.OffiAccount.AppID,     // 小程序、公众号或者企业微信的appid
+		Secret: conf.OffiAccount.AppSecret, // 商户号 appID
 
-    ResponseType: os.Getenv("response_type"),
-    Log: officialAccount.Log{
-      Level: "debug",
-      File:  "./wechat.log",
-    },
-    Cache:     cache,
-    HttpDebug: true,
-    Debug:     false,
-    //"sandbox": true,
-  })
+		Token:  "xxx",
+		AESKey: "xxx",
 
-  return app, err
+		ResponseType: os.Getenv("response_type"),
+		Log: officialAccount.Log{
+			Level: "debug",
+			File:  "./wechat.log",
+		},
+		Cache:     cache,
+		HttpDebug: true,
+		Debug:     false,
+		//"sandbox": true,
+	})
+
+	return app, err
 }
