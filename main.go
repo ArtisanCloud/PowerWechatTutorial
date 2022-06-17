6ac767b049f3ebd07745f9bd49848e8bd2961d66
Walle <walle@artisan-cloud.com>
2022-06-17 23:47:34 +0800

    feat(offcial): add Token and AES config

--- main.go
+++ main.go
@@ -1,50 +1,50 @@
 package main
 
 import (
-  "github.com/gin-gonic/gin"
-  "log"
-  "power-wechat-tutorial/config"
-  "power-wechat-tutorial/routes"
-  "power-wechat-tutorial/services"
+	"github.com/gin-gonic/gin"
+	"log"
+	"power-wechat-tutorial/config"
+	"power-wechat-tutorial/routes"
+	"power-wechat-tutorial/services"
 )
 
 var Host = ""
 var Port = "8888"
 
 func main() {
-  conf := config.Get()
+	conf := config.Get()
 
-  var err error
-  services.PaymentApp, err = services.NewWXPaymentApp(conf)
-  if err != nil || services.PaymentApp == nil {
-    panic(err)
-  }
+	var err error
+	services.PaymentApp, err = services.NewWXPaymentApp(conf)
+	if err != nil || services.PaymentApp == nil {
+		panic(err)
+	}
 
-  services.MiniProgramApp, err = services.NewMiniMiniProgramService(conf)
-  if err != nil || services.MiniProgramApp == nil {
-    panic(err)
-  }
+	services.MiniProgramApp, err = services.NewMiniMiniProgramService(conf)
+	if err != nil || services.MiniProgramApp == nil {
+		panic(err)
+	}
 
-  services.OfficialAccountApp, err = services.NewOfficialAccountAppService(conf)
-  if err != nil || services.OfficialAccountApp == nil {
-    panic(err)
-  }
+	services.OfficialAccountApp, err = services.NewOfficialAccountAppService(conf)
+	if err != nil || services.OfficialAccountApp == nil {
+		panic(err)
+	}
 
-  services.WeComApp, err = services.NewWeComService(conf)
-  if err != nil || services.WeComApp == nil {
-    panic(err)
-  }
+	services.WeComApp, err = services.NewWeComService(conf)
+	if err != nil || services.WeComApp == nil {
+		panic(err)
+	}
 
-  services.WeComContactApp, err = services.NewWeComContactService(conf)
-  if err != nil || services.WeComContactApp == nil {
-    panic(err)
-  }
+	services.WeComContactApp, err = services.NewWeComContactService(conf)
+	if err != nil || services.WeComContactApp == nil {
+		panic(err)
+	}
 
-  r := gin.Default()
+	r := gin.Default()
 
-  // Initialize the routes
-  routes.InitializeRoutes(r)
+	// Initialize the routes
+	routes.InitializeRoutes(r)
 
-  log.Fatalln(r.Run(Host + ":" + Port))
+	log.Fatalln(r.Run(Host + ":" + Port))
 
 }
