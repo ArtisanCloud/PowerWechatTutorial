41a2c66cb638c58eeb4efd2d60bc300947b12087
Walle <walle@artisan-cloud.com>
2022-06-28 23:32:24 +0800

    feat(wecom): add miniprogram code2Session

--- services/wecom-service.go
+++ services/wecom-service.go
@@ -1,62 +1,61 @@
 package services
 
 import (
-  "github.com/ArtisanCloud/PowerWeChat/v2/src/kernel"
-  "github.com/ArtisanCloud/PowerWeChat/v2/src/work"
-  "power-wechat-tutorial/config"
+	"github.com/ArtisanCloud/PowerWeChat/v2/src/kernel"
+	"github.com/ArtisanCloud/PowerWeChat/v2/src/work"
+	"power-wechat-tutorial/config"
 )
 
 var WeComApp *work.Work
 var WeComContactApp *work.Work
 
 func NewWeComService(conf *config.Configuration) (*work.Work, error) {
-  var cache kernel.CacheInterface
-  if conf.WeCom.RedisAddr != "" {
-    cache = kernel.NewRedisClient(&kernel.RedisOptions{
-      Addr: conf.Payment.RedisAddr,
-    })
-  }
+	var cache kernel.CacheInterface
+	if conf.WeCom.RedisAddr != "" {
+		cache = kernel.NewRedisClient(&kernel.RedisOptions{
+			Addr: conf.Payment.RedisAddr,
+		})
+	}
 
-  app, err := work.NewWork(&work.UserConfig{
-    CorpID:      conf.WeCom.CorpID,          // 企业微信的corp id，所有企业微信共用一个。
-    AgentID:     conf.WeCom.AgentID,         // 内部应用的app id
-    Secret:      conf.WeCom.Secret,          // 内部应用的app secret
-    Token:       conf.WeCom.MessageToken,    // 内部应用的app token
-    AESKey:      conf.WeCom.MessageAesKey,   // 内部应用的app aeskey
-    CallbackURL: conf.WeCom.MessageCallback, // 内部应用的场景回调设置
-    OAuth: work.OAuth{
-      Callback: conf.WeCom.OAuthCallback, // 内部应用的app oauth url
-      Scopes:   []string{"snsapi_base"},
-    },
-    Cache:     cache,
-    HttpDebug: true,
-  })
+	app, err := work.NewWork(&work.UserConfig{
+		CorpID:      conf.WeCom.CorpID,          // 企业微信的corp id，所有企业微信共用一个。
+		AgentID:     conf.WeCom.AgentID,         // 内部应用的app id
+		Secret:      conf.WeCom.Secret,          // 内部应用的app secret
+		Token:       conf.WeCom.MessageToken,    // 内部应用的app token
+		AESKey:      conf.WeCom.MessageAesKey,   // 内部应用的app aeskey
+		CallbackURL: conf.WeCom.MessageCallback, // 内部应用的场景回调设置
+		OAuth: work.OAuth{
+			Callback: conf.WeCom.OAuthCallback, // 内部应用的app oauth url
+			Scopes:   []string{"snsapi_base"},
+		},
+		Cache:     cache,
+		HttpDebug: true,
+	})
 
-  return app, err
+	return app, err
 }
 
 func NewWeComContactService(conf *config.Configuration) (*work.Work, error) {
-  var cache kernel.CacheInterface
-  if conf.MiniProgram.RedisAddr != "" {
-    cache = kernel.NewRedisClient(&kernel.RedisOptions{
-      Addr: conf.Payment.RedisAddr,
-    })
-  }
+	var cache kernel.CacheInterface
+	if conf.MiniProgram.RedisAddr != "" {
+		cache = kernel.NewRedisClient(&kernel.RedisOptions{
+			Addr: conf.Payment.RedisAddr,
+		})
+	}
 
-  app, err := work.NewWork(&work.UserConfig{
-    CorpID:      conf.WeCom.CorpID,          // 企业微信的app id，所有企业微信共用一个。
-    AgentID:     conf.WeCom.AgentID,         // 内部应用的app id
-    Secret:      conf.WeCom.Secret,          // 内部应用的app secret
-    Token:       conf.WeCom.ContactToken,    // 内部应用的app token
-    AESKey:      conf.WeCom.ContactAESKey,   // 内部应用的app aeskey
-    CallbackURL: conf.WeCom.ContactCallback, // 内部应用的场景回调设置
-    OAuth: work.OAuth{
-      Callback: conf.WeCom.OAuthCallback, // 内部应用的app oauth url
-      Scopes:   nil,
-    },
-    HttpDebug: true,
-    Cache:     cache,
-  })
-
-  return app, err
+	app, err := work.NewWork(&work.UserConfig{
+		CorpID:      conf.WeCom.CorpID,          // 企业微信的app id，所有企业微信共用一个。
+		AgentID:     conf.WeCom.AgentID,         // 内部应用的app id
+		Secret:      conf.WeCom.Secret,          // 内部应用的app secret
+		Token:       conf.WeCom.ContactToken,    // 内部应用的app token
+		AESKey:      conf.WeCom.ContactAESKey,   // 内部应用的app aeskey
+		CallbackURL: conf.WeCom.ContactCallback, // 内部应用的场景回调设置
+		OAuth: work.OAuth{
+			Callback: conf.WeCom.OAuthCallback, // 内部应用的app oauth url
+			Scopes:   nil,
+		},
+		HttpDebug: true,
+		Cache:     cache,
+	})
+	return app, err
 }
